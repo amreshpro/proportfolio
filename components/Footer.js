80824575f7979cd01b1c5722647efb5afba94d25
Amresh Maurya <amresh.terminal@gmail.com>
2023-12-12 12:48:43 +0530

    update the footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,44 +1,48 @@
-"use client"
+"use client";
 
-import { socialLinks } from "@/constants"
-import Link from "next/link"
-import { BsGithub, BsInstagram, BsLinkedin } from "react-icons/bs"
-
+import { socialLinks } from "@/constants";
+import Link from "next/link";
+import { BsGithub, BsInstagram, BsLinkedin } from "react-icons/bs";
 
 export const Footer = () => {
   return (
-    <footer className="flex flex-col items-center ">
-      <div className="container px-6 pt-6">
-        <div className="mb-6 flex gap-4 justify-center">
+    <footer className="mt-4 flex flex-col items-center ">
+      <h1 className="text-3xl sm:text-2xl font-bold text-center gradient-text ">Connect With Me</h1>
+            <div className="mt-6 flex flex-wrap gap-4 justify-center">
           <Link
             href={socialLinks.linkedin}
-            className="rounded-full bg-lightBlue "
+            target="_blank"
+            className="px-4 py-2 rounded-l-full rounded-r-full bg-[#0077b5] text-white text-lg flex gap-2 items-center"
           >
-            <BsLinkedin className="rounded-full text-4xl" />
-          </Link>
-
-          <Link
-            href={socialLinks.github}
-            className="rounded-full  "
-          >
-            <BsGithub className="rounded-full text-4xl" />
+            <p className="text-lg font-bold">Linkedin</p>
+            <BsLinkedin className="rounded-lg text-3xl" />
           </Link>
 
           <Link
             href={socialLinks.instagram}
-            className="rounded-full bg-carrot "
+            target="_blank"
+            className="px-4 py-2 rounded-l-full rounded-r-full bg-[#e4405f] text-white text-lg flex gap-2 items-center"
           >
-            <BsInstagram className="rounded-full text-4xl " />
+            <p className="text-lg font-bold">Instagram</p>
+            <BsInstagram className="rounded-lg text-3xl" />
+          </Link>
+          <Link
+            href={socialLinks.github}
+            target="_blank"
+            className="px-4 py-2 rounded-l-full rounded-r-full bg-[#2b3137] text-white text-lg flex gap-2 items-center"
+          >
+            <p className="text-lg font-bold">Github</p>
+            <BsGithub className="rounded-lg text-3xl" />
           </Link>
         </div>
-      </div>
+     
 
       <div className="w-full p-4 text-center">
         © 2023 Copyright:
-        <Link className="text-whitehite" href={socialLinks.portfolio}>
+        <Link className="link-hover font-semibold" href={socialLinks.portfolio}>
           amreshpro
         </Link>
       </div>
     </footer>
   );
-}
+};

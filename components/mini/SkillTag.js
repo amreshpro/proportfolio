59f789fc49f8485596e803785b8662bd7c6f647a
Amresh Maurya <amresh.terminal@gmail.com>
2023-12-12 22:26:43 +0530

    fix margin issue

--- components/mini/SkillTag.js
+++ components/mini/SkillTag.js
@@ -2,7 +2,7 @@ export default function SkillTag({name,image_url}) {
   return (
     <div className=" m-2 px-4 py-1 rounded-r-full rounded-l-full flex justify-center items-center gap-2 bg-[#d2e0fb] text-black">
       <img src={image_url} alt="name" className="w-6 " />
-      <h1 className="font-mono mt-1">{name}</h1>
+      <h1 className="font-mono mt-0.5">{name}</h1>
     </div>
   )
 }

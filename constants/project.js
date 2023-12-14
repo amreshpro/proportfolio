6c1b0e47f9d3c4601212562dcbaa913f2ec4dfc6
Amresh Maurya <amresh.terminal@gmail.com>
2023-12-14 11:58:59 +0530

    Update project.js

--- constants/project.js
+++ constants/project.js
@@ -66,7 +66,7 @@ export const othersContribution = [
 export const clones = [
   {
     id: "clones_1",
-    title: "Swigg Clone",
+    title: "Swiggy Clone",
     description: `Developed a fully functional clone of the Swiggy.com website using modern web technologies to replicate its core features and user interface.
   Technologies/Tools Used: React.js, Tailwind CSS, JavaScript, HTML, CSS, Git, and GitHub.`,
     image_url: "https://i.imgur.com/LufubPv.png",

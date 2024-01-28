0a95c3c7e68915c4180ba9a7a7f3a3354f543a24
Monika <monika.jsx@gmail.com>
2024-01-28 22:53:57 +0530

    update hero section

--- src/components/ProfileContent.jsx
+++ src/components/ProfileContent.jsx
@@ -1,7 +1,7 @@
 import { FaLinkedin } from "react-icons/fa";
 import { FiGithub } from "react-icons/fi";
 import { GiNotebook } from "react-icons/gi";
-import { MYBIO, MYPROFILE } from "../constants/INFO";
+import { MYNAME, MYPROFILE } from "../constants/INFO";
 
 import { socialLinks } from "../constants/SOCIAL_LINKS";
 import { Link } from "react-router-dom";
@@ -12,10 +12,10 @@ export default function ProfileContent() {
   const { theme } = useContext(ThemeContext);
 
   return (
-    <div className="content w-1/2 sm:w-full px-8 sm:px-2 sm:py-2 pt-8 flex flex-col gap-8 ">
-      <p className="tracking-wide text-justify text-5xl sm:text-3xl font-bold sm:text-start">
-        {MYBIO}
-      </p>
+    <div className="content w-1/2 sm:w-full px-8 sm:px-2 sm:py-2 pt-8 flex flex-col sm:justify-center sm:items-center sm:gap-2 gap-8 ">
+      <h1 className="tracking-wide text-justify text-5xl sm:text-3xl font-bold ">
+        {MYNAME}
+      </h1>
       <h1 className="text-3xl sm:text-xl font-bold ">{MYPROFILE}</h1>
       <div className="social-links flex items-center sm:justify-center gap-3 text-3xl">
         <Link
--- src/constants/INFO.jsx
+++ src/constants/INFO.jsx
@@ -1,4 +1,4 @@
-export const MYBIO = `Amresh Maurya`;
+export const MYNAME = `Amresh Maurya`;
 
 // profile
 export const MYPROFILE = "React Developer | Frontend Developer";

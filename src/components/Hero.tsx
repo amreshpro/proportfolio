98aaa22dc4b5e22bd8c17694b56e1cc0c4097513
Amresh Maurya <amresh.terminal@gmail.com>
2024-04-06 23:14:27 +0530

    hero section fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,17 @@
 export default function Hero() {
   return (
-    <div className="bg-white text-[#111827] p-4 rounded-lg border-1 border-4 border-[#014fff] shadow-lg shadow-[#014fff] ">
-        <div className="name flex  justify-center flex-wrap gap-2  text-6xl md:text-5xl sm:text-4xl font-bold">
+    <div className="flex flex-col justify-center items-center gap-4  bg-white  text-[#111827] p-8 rounded-lg border-1 border-4 border-[#014fff] shadow-lg shadow-[#014fff] ">
+   
+   <div className="image  ">
+    <img src="/amresh.jpg" alt="my-photo" className=" object-cover w-[250px] h-[250px] rounded-full"  />
+   </div>
+      <div className="name flex items-center  justify-center flex-wrap gap-2  text-6xl md:text-5xl sm:text-4xl font-bold">
         <h1>Hii, I am</h1>
-        <h1 className="text-[#014FFF]">Amresh Maurya</h1>
-        </div>
+        <h1 className="text-[#014fff]">Amresh Maurya</h1>
+      </div>
+      <div className="profile">
+        <h1 className="font-medium text-2xl text-[#2b4b96] ">React Developer | Frontend Developer</h1>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/LayoutContainer.tsx
+++ src/components/LayoutContainer.tsx
@@ -1,7 +1,9 @@
 import { ReactNode } from "@tanstack/react-router";
 
-export default function LayoutContainer({children}:{children:ReactNode}) {
+export default function LayoutContainer({ children }: { children: ReactNode }) {
   return (
-    <main className="flex justify-center md:p-8 sm:p-2 sm:pt-8 ">{children}</main>
-  )
+    <main className="flex justify-center p-8 md:p-6 sm:p-2  sm:pt-8 ">
+      {children}
+    </main>
+  );
 }

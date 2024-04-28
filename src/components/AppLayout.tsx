dd9042dcea56c795358f92ced9c1792425c0934d
Amresh Maurya <amresh.terminal@gmail.com>
2024-04-28 17:01:54 +0530

    fix issue

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import AllSection from "./AllSection";
+
 
 export default function AppLayout() {
   return (

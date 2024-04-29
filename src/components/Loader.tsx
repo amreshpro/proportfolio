2d33c65d73e2469aaef935b453f226dd8a6a0874
Amresh Maurya <amresh.terminal@gmail.com>
2024-04-29 07:55:48 +0530

    update loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,11 +1,11 @@
+import { TbLoader2 } from "react-icons/tb";
+
 export default function Loader() {
   return (
-    <div className=" w-screen h-screen fixed border border-blue-300 shadow rounded-md p-4 max-w-sm  mx-auto">
-      <div className="ternary animate-pulse w-full h-full flex justify-center items-center flex-wrap gap-4 p-4">
-   
-        <div className="box box-1 animate-spin  bg-teal-300 w-10 h-10"></div>
-        <div className="box box-1   animate-spin bg-pink-500 w-10 h-10"></div>
-        <div className="box box-1  animate-spin  bg-yellow-200 w-10 h-10"></div>
+    <div className=" w-screen h-screen fixed">
+      <div className="ternary animate-pulse w-full h-full flex justify-center items-center ">
+   <TbLoader2 className="text-5xl animate-spin"/>
+
       </div>
     </div>
   );

f81d4401d8cdd03c56f61d00ad73ca7b0607bb9b
Amresh Maurya <amresh.terminal@gmail.com>
2024-05-24 19:10:07 +0530

    basic setup of app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,18 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+
+    screens: {
+      '2xl': { 'max': '1535px' },
+      'xl': { 'max': '1279px' },
+      'lg': { 'max': '1023px' },
+      'md': { 'max': '767px' },
+      'sm': { 'max': '639px' },
+      'xsm': { 'max': '339px' },
+    },
+
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+     
     },
   },
   plugins: [],

965e1a4a973105c344eab07817c1ac30b7fb5547
Amresh Maurya <amresh.terminal@gmail.com>
2024-06-08 22:47:50 +0530

    remove textutils project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,12 +1,15 @@
-"use client"
+"use client";
 /* eslint-disable @next/next/no-img-element */
-import { PROJECTS, OTHERS_PROJECTS, ZSH_THEME, REACT_PROJECTS } from "../data/PROJECTS";
+import { PROJECTS, OTHERS_PROJECTS, ZSH_THEME } from "../data/PROJECTS";
 import ProjectCard from "./ui/ProjectCard";
 
 const Projects = () => {
   return (
     <div className="mt-4 flex  flex-col justify-center items-center px-4 w-full ">
-      <h1 id="projects" className=" uppercase w-fit p-2 px-6 rounded-lg  text-2xl text-center font-bold">
+      <h1
+        id="projects"
+        className=" uppercase w-fit p-2 px-6 rounded-lg  text-2xl text-center font-bold"
+      >
         Projects
       </h1>
       <div className="my-10  flex flex-wrap justify-center gap-8 sm:gap-8">
@@ -21,29 +24,7 @@ const Projects = () => {
           return <ProjectCard {...project} key={project?.id} />;
         })}
       </div>
-
-      <h1 id="projects" className=" uppercase w-fit p-2 px-6 rounded-lg  text-2xl text-center font-bold">
-        React.js Projects
-      </h1>
-      <div className="my-10  flex flex-wrap justify-center gap-8 sm:gap-8">
-     
-        {REACT_PROJECTS.map((project) => {
-          return <ProjectCard {...project} key={project?.id} />;
-        })}
-
-        
-      </div>
-    
-
     </div>
   );
 };
 export default Projects;
-
-
-
-
-
-
-
-

a647f444ba4af6cc36ff7d895410ab300fd0078b
Amresh Maurya <amresh.terminal@gmail.com>
2024-07-16 20:50:46 +0530

    update skills and logo

--- data/SKILLS.ts
+++ data/SKILLS.ts
@@ -1,14 +1,17 @@
-
-
 export const PROGRAMMING_LANGUAGES = [
   {
-    id:"pl-1",
-    name:"Javascript",
+    id: "pl-1",
+    name: "Javascript",
     image_url: "/skills/js.png",
   },
   {
-    id:"pl-2",
-    name:"Python",
+    id: "pl-2",
+    name: "Java",
+    image_url: "/skills/java.svg",
+  },
+  {
+    id: "pl-3",
+    name: "Python",
     image_url: "/skills/python.png",
   },
   // {
@@ -16,27 +19,21 @@ export const PROGRAMMING_LANGUAGES = [
   //   name:"Go",
   //   image_url: "/skills/go.svg",
   // },
+];
 
-
-]
-
-
-export const FRONTEND_TECHNOLOGY=[
+export const FRONTEND_TECHNOLOGY = [
+ 
+  {
+    id: "techidnextjs",
+    name: "Nextjs",
+    image_url: "/skills/nextjs.png",
+  },
   {
     id: "reactid",
     name: "React",
     image_url: "/skills/react.png",
   },
-  {
-    id: "techidnextjs",
-    name: "Nextjs",
-    image_url: "/skills/nextjs.png",
-  },{
-    id:"zustand",
-    name:"Zustand",
-    image_url:"/skills/zustand.jpeg"
-  },
-  
+ 
   {
     id: "tailwind",
     name: "Tailwind",
@@ -52,130 +49,120 @@ export const FRONTEND_TECHNOLOGY=[
     name: "HTML5",
     image_url: "/skills/html.png",
   },
-]
+];
 
-export const BACKEND_TECHNOLOGY=[
+export const BACKEND_TECHNOLOGY = [
   {
-    id:"expressjs",
-    name:"Express",
-    image_url:"/skills/expressjs.png"
-  },{
-    id:"django",
-    name:"Django",
-    image_url:"/skills/django.png"
-  }
-]
-
+    id: "expressjs",
+    name: "Express",
+    image_url: "/skills/expressjs.svg",
+  },
+  {
+    id: "spring",
+    name: "Spring",
+    image_url: "/skills/spring.svg",
+  },
+  // {
+  //   id:"django",
+  //   name:"Django",
+  //   image_url:"/skills/django.png"
+  // }
+];
 
 export const SKILLS = [
-    {
-      id: "techidhtml",
-      name: "HTML5",
-      // image_url: "https://i.imgur.com/whCcsR1.png",
-      image_url: "/skills/html.png",
-    },
-    {
-      id: "techidcss",
-      name: "CSS3",
-  
-      //  image_url: "https://i.imgur.com/eCUFQLX.png"
-      image_url: "/skills/css.png",
-    },
-    {
-      id: "techidtailwindcss",
-      name: "Tailwind",
-      // image_url: "https://i.imgur.com/6lizxkE.png",
-      image_url: "/skills/tailwind.png",
-    },
-    {
-      id: "techidjs",
-      name: "JavaScript",
-      // image_url: "https://i.imgur.com/2EeGxbg.png",
-      image_url: "/skills/js.png",
-    },
-    {
-      id: "techidtypescript",
-      name: "TypeScript",
-      // image_url: "https://i.imgur.com/27qaz8a.png",
-      image_url: "/skills/typescript.png",
-    },
-    {
-      id: "techidreact",
-      name: "React",
-      // image_url: "https://i.imgur.com/3XlqowC.png",
-      image_url: "/skills/react.png",
-    },
-  
-    {
-      id: "techidexpress",
-      name: "Express",
-      image_url: "/skills/expressjs.png",
-    },
-    {
-      id: "techidnextjs",
-      name: "Nextjs",
-      image_url: "/skills/nextjs.png",
-    },
-    {
-      id: "techidredux",
-      name: "Redux",
-      // image_url: "https://i.imgur.com/lxzmhyH.png",
-      image_url: "/skills/redux.png",
-    },
-  
-  
-    {
-      id: "techidnodejs",
-      name: "Node",
-      // image_url: "https://i.imgur.com/B8NJvnF.png",
-      image_url: "/skills/nodejs.png",
-    },
-    {
-      id: "techidmongodb",
-      name: "MongoDB",
-      // image_url: "https://i.imgur.com/RVCqVh8.png",
-      image_url: "/skills/mongodb.png",
-    },
-    // {
-    //   id: "techidthree",
-    //   name: "Three JS",
-    //   image_url: "https://i.imgur.com/Lky1TeM.jpg",
-    // },
-  ];
-  
-  export const OTHER_SKILLS = [
-    {
-      id: "techidpostman",
-      name: "Postman",
-      image_url: "/skills/postman.svg",
-    },
-    {
-      id: "techidvscode",
-      name: "VSCode",
-      image_url: "/skills/vscode.png",
-    },
-    {
-      id: "techidgit",
-      name: "Git",
-      image_url: "/skills/git.png",
-    },
-    {
-      id: "techidgithub",
-      name: "Github",
-       image_url: "/skills/github.png",
-    },
-    {
-      id: "techidlinux",
-      name: "Linux",
-      image_url: "/skills/linux.png",
-    },
-    {
-      id: "techidocker",
-      name: "Docker",
-      image_url: "/skills/docker.png",
-    },
-  ];
-  
-  
-  
-  
+  {
+    id: "techidhtml",
+    name: "HTML5",
+    image_url: "/skills/html.png",
+  },
+  {
+    id: "techidcss",
+    name: "CSS3",
+    image_url: "/skills/css.png",
+  },
+  {
+    id: "techidtailwindcss",
+    name: "Tailwind",
+    image_url: "/skills/tailwind.png",
+  },
+  {
+    id: "techidjs",
+    name: "JavaScript",
+    image_url: "/skills/js.png",
+  },
+  {
+    id: "techidtypescript",
+    name: "TypeScript",
+    image_url: "/skills/typescript.png",
+  },
+  {
+    id: "techidreact",
+    name: "React",
+    image_url: "/skills/react.png",
+  },
+
+  {
+    id: "techidexpress",
+    name: "Express",
+    image_url: "/skills/expressjs.png",
+  },
+  {
+    id: "techidnextjs",
+    name: "Nextjs",
+    image_url: "/skills/nextjs.png",
+  },
+  {
+    id: "techidredux",
+    name: "Redux",
+    image_url: "/skills/redux.png",
+  },
+
+  {
+    id: "techidnodejs",
+    name: "Node",
+    image_url: "/skills/nodejs.png",
+  },
+  {
+    id: "techidmongodb",
+    name: "MongoDB",
+    image_url: "/skills/mongodb.png",
+  },
+  // {
+  //   id: "techidthree",
+  //   name: "Three JS",
+  //   image_url: "https://i.imgur.com/Lky1TeM.jpg",
+  // },
+];
+
+export const OTHER_SKILLS = [
+  {
+    id: "techidpostman",
+    name: "Postman",
+    image_url: "/skills/postman.svg",
+  },
+  {
+    id: "techidvscode",
+    name: "VSCode",
+    image_url: "/skills/vscode.png",
+  },
+  {
+    id: "techidgit",
+    name: "Git",
+    image_url: "/skills/git.png",
+  },
+  {
+    id: "techidgithub",
+    name: "Github",
+    image_url: "/skills/github.png",
+  },
+  {
+    id: "techidlinux",
+    name: "Linux",
+    image_url: "/skills/ubuntu.svg",
+  },
+  {
+    id: "techidocker",
+    name: "Docker",
+    image_url: "/skills/docker.png",
+  },
+];

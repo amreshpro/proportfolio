382e8f1c78b8b4336717fe2bac13e8595b4a8d55
Amresh Maurya <amresh.terminal@gmail.com>
2024-07-26 22:06:18 +0530

    update light colrs

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -22,15 +22,12 @@ export default function Navbar() {
       <nav
         className={`${
           isDarkModeEnabled ? "dark-box" : "light-box"
-        } z-50 fixed sm:hidden  border mt-2 sm:mt-0 border-gray-300 px-4 py-4 flex justify-between gap-4 sm:gap-1 items-center rounded-r-full rounded-l-full  `}
+        } z-50 fixed sm:hidden   border mt-2 sm:mt-0 border-gray-300 px-4 py-4 flex justify-between gap-4 sm:gap-1 items-center rounded-r-full rounded-l-full  `}
       >
         <div className="logo flex sm:gap-0.5 gap-2 px-0.5 ">
-         
-            <p className="bg-red-400 rounded-full p-2 sm:p-1  hover:animate-bounce"></p>
-            <p className="bg-orange-400 rounded-full p-2 sm:p-1 hover:animate-bounce "></p>
-            <p className="bg-green-400 rounded-full p-2 sm:p-1 hover:animate-bounce"></p>
-        
-        
+          <p className="bg-red-400 rounded-full p-2 sm:p-1  hover:animate-bounce"></p>
+          <p className="bg-orange-400 rounded-full p-2 sm:p-1 hover:animate-bounce "></p>
+          <p className="bg-green-400 rounded-full p-2 sm:p-1 hover:animate-bounce"></p>
         </div>
         <ul className="flex gap-8 sm:gap-2 md:gap-4 items-center sm:text-sm">
           {NAV_LINKS.map((nav) => {
@@ -58,9 +55,9 @@ export default function Navbar() {
       </nav>
 
       {/* mobile menu */}
-      <nav className="mobile-menu hidden sm:flex  w-screen  z-50">
+      <aside className="mobile-menu hidden sm:flex   w-screen  z-50">
         <MobileMenu />
-      </nav>
+      </aside>
     </>
   );
 }

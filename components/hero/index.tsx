59104d39fa26aca0e78785d7e05b55ee414b7b28
Amresh Maurya <amresh.terminal@gmail.com>
2024-09-22 22:38:36 +0530

    add hero ring

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -4,14 +4,16 @@ import SOCIAL_MEDIA_LINKS from "@/data/SOCIAL_MEDIA_LINKS";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import Link from "next/link";
 import { twMerge } from "tailwind-merge";
+import Orbit from "../orbit";
 
 export default function Hero() {
   return (
     <div className=" max-w-2xl min-h-[95vh] sm:h-auto  flex flex-col  gap-4 items-center justify-center text-center mx-auto">
+  <Orbit/>
       <h1 className="text-2xl">Hii, I am </h1>
       {/* Title */}
 
-      <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-pink-500  to-orange-500  scroll-m-20 sm:text-4xl md:text-5xl lg:text-7xl font-extrabold tracking-tight text-5xl">
+      <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-pink-500  to-orange-500  scroll-m-20 sm:text-5xl md:text-6xl text-7xl font-extrabold tracking-tight">
         Amresh Maurya
       </h1>
 
@@ -22,15 +24,20 @@ export default function Hero() {
       <div className="mt-8 gap-3 flex sm:flex-wrap justify-center">
         <Link
           href={SOCIAL_MEDIA_LINKS.github}
-          className={twMerge( buttonVariants({ variant: "default" }),"bg-gray-800/90 rounded-full")}
+          className={twMerge(
+            buttonVariants({ variant: "default" }),
+            "bg-gray-800/90 rounded-full"
+          )}
         >
           <p className="text-xl px-4">Github</p>
           <FaGithub className="text-2xl" />
-
         </Link>
         <Link
           href={SOCIAL_MEDIA_LINKS.linkedin}
-          className={twMerge( buttonVariants({ variant: "default" }),"bg-gray-800/90 rounded-full")}
+          className={twMerge(
+            buttonVariants({ variant: "default" }),
+            "bg-gray-800/90 rounded-full"
+          )}
         >
           <p className="text-xl px-4">Linkedin</p>
           <FaLinkedin className="text-2xl" />

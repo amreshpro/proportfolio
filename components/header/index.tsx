54c450dc86b5c16339d024109aaa03fb8992d39c
Amresh Maurya <amresh.terminal@gmail.com>
2024-11-07 23:55:49 +0530

    navbar shift in bottom

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="flex justify-center items-center fixed top-3 w-full">
+    <div className="flex justify-center items-center fixed bottom-3 w-full">
       <nav className="flex gap-1 p-0.5 px-4 py-2 border border-secondary/15 rounded-full bg-secondary backdrop-blur">
         {NAV_LINKS?.map((link) => {
           return (

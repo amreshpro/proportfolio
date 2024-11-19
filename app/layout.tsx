3cb2aa90ebba533e8d35b7958d98f78b96b27e30
Amresh Maurya <amresh.terminal@gmail.com>
2024-11-19 13:37:19 +0530

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="shortcut icon" href="./" type="image/x-icon" />
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon-192x192.png" />
+        <meta name="description" content="Your website description here" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
